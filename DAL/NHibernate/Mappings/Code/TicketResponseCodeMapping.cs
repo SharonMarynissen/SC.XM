1cd18680d5cdfd7cce9390fad723fc4522056878
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-03-08 20:41:25 +0100

    Added mapping for TicketResponse (Code mapping), NOT TESTED YET!

---
+++ DAL/NHibernate/Mappings/Code/TicketResponseCodeMapping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using SC.BL.Domain;
+
+namespace SC.DAL.NHibernate.Mappings.Code
+{
+    public class TicketResponseCodeMapping:ClassMapping<TicketResponse>
+    {
+        public TicketResponseCodeMapping()
+        {
+            Id(tr=>tr.Id, mapper =>
+            {
+                mapper.Column("Id");
+                mapper.Generator(Generators.Increment);
+            });
+            Property(tr=>tr.Text, mapper =>
+            {
+                mapper.Column("Text");
+                mapper.NotNullable(false);
+                mapper.Type(NHibernateUtil.String);
+            });
+            Property(tr=>tr.Date, mapper =>
+            {
+                mapper.Column("Date");
+                mapper.NotNullable(false);
+                mapper.Type(NHibernateUtil.DateTime);
+            });
+            Property(tr=>tr.IsClientResponse, mapper =>
+            {
+                mapper.Column("IsClientResponse");
+                mapper.NotNullable(false);
+                mapper.Type(NHibernateUtil.Boolean);
+            });
+            ManyToOne<Ticket>(tr=>tr.Ticket, mapper =>
+            {
+                mapper.Column("Ticket_id");
+                mapper.NotNullable(false);
+            });
+        }
+    }
+}
