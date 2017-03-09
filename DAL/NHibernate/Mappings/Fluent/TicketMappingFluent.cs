2374956b85bac26d4716f92679b2215c94e2dae1
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-03-09 20:33:42 +0100

    added fluent mapping, not tested yet!

---
+++ DAL/NHibernate/Mappings/Fluent/TicketMappingFluent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SC.BL.Domain;
+
+namespace SC.DAL.NHibernate.Mappings.Fluent
+{
+    public class TicketMappingFluent : ClassMap<Ticket>
+    {
+        public TicketMappingFluent()
+        {
+            Id(t => t.TicketNumber, "TicketNumber").GeneratedBy.Native();
+            Map(t => t.AccountId, "AccountId").Nullable();
+            Map(t => t.Text, "Text").Nullable();
+            Map(t => t.DateOpened, "DateOpened").Nullable();
+            Map(t => t.State, "State").Nullable();
+            HasMany(t => t.Responses)
+                .Cascade.SaveUpdate()
+                .KeyColumn("Ticket_id")
+                .Inverse();
+        }
+    }
+
+    public class HardwareTicketFluentMapping : SubclassMap<HardwareTicket>
+    {
+        public HardwareTicketFluentMapping()
+        {
+            DiscriminatorValue("HT");
+            Map(ht => ht.DeviceName, "DeviceName").Nullable();
+        }
+    }
+}
---
+++ DAL/NHibernate/Mappings/Fluent/TicketResponseFluentMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SC.BL.Domain;
+
+namespace SC.DAL.NHibernate.Mappings.Fluent
+{
+    public class TicketResponseFluentMapping : ClassMap<TicketResponse>
+    {
+        public TicketResponseFluentMapping()
+        {
+            Id(tr => tr.Id, "Id").GeneratedBy.Increment();
+            Map(tr => tr.Text, "Text").Nullable();
+            Map(tr => tr.Date, "Date").Nullable()
+                .CustomType(typeof(DateTime));
+            Map(tr => tr.IsClientResponse, "IsClientResponse").Nullable()
+                .CustomType(typeof(bool));
+            References<Ticket>(tr => tr.Ticket, "Ticket_id");
+        }
+    }
+}
